40084c720cb342bde2c5ecd70cfc928d2f2bd632
Paula Muldoon <pmuldoon@umich.edu>
2017-07-10 13:10:18 +0100

    all number tests added

--- lib/presenter.rb
+++ lib/presenter.rb
@@ -1,18 +1,47 @@
 class Presenter
 
-  def display(number)
-    check_suite(number)
+  def display(n)
+    'You have the ' + check_number(n) + ' of ' + check_suite(n)
   end
 
   private
 
+  def check_number(n)
+    if n == 0 || n == 13 || n == 26 || n == 39
+      'ace'
+    elsif n == 1 || n == 14 || n == 27 || n == 40
+      'two'
+    elsif n == 2 || n == 15 || n == 28 || n == 41
+      'three'
+    elsif n == 3 || n == 16 || n == 29 || n == 42
+      'four'
+    elsif n == 4 || n == 17 || n == 30 || n == 43
+      'five'
+    elsif n == 5 || n == 18 || n == 31 || n == 44
+      'six'
+    elsif n == 6 || n == 19 || n == 32 || n == 45
+      'seven'
+    elsif n == 7 || n == 20 || n == 33 || n == 46
+      'eight'
+    elsif n == 8 || n == 21 || n == 34 || n == 47
+      'nine'
+    elsif n == 9 || n == 22 || n == 35 || n == 48
+      'ten'
+    elsif n == 10 || n == 23 || n == 36 || n == 49
+      'jack'
+    elsif n == 11 || n == 24 || n == 37 || n == 50
+      'queen'
+    elsif n == 12 || n == 25 || n == 38 || n == 51
+      'king'
+    end
+  end
 
-  def check_suite(number)
-    if number > -1 && number < 13
+  def check_suite(n)
+    if n > -1 && n < 13
       'hearts'
-    elsif number > 12 && number < 26
+    elsif n > 12 && n < 26
       'clubs'
-    elsif number > 25 && number < 39
+    elsif n > 25 && n < 39
       'spades'
     else
       'diamonds'
--- spec/presenter_spec.rb
+++ spec/presenter_spec.rb
@@ -8,7 +8,7 @@ describe Presenter do
     it 'should know when a card is a heart' do
       n = 0
       while n < 13
-        expect(presenter.display(n)).to eq 'hearts'
+        expect(presenter.display(n)).to include 'hearts'
         n += 1
       end
     end
@@ -16,7 +16,7 @@ describe Presenter do
     it 'should know when a card is a club' do
       n = 13
       while n < 26
-        expect(presenter.display(n)).to eq 'clubs'
+        expect(presenter.display(n)).to include 'clubs'
         n += 1
       end
     end
@@ -24,7 +24,7 @@ describe Presenter do
     it 'should know when a card is a spade' do
       n = 26
       while n < 39
-        expect(presenter.display(n)).to eq 'spades'
+        expect(presenter.display(n)).to include 'spades'
         n += 1
       end
     end
@@ -32,8 +32,114 @@ describe Presenter do
     it 'should know when a card is a diamond' do
       n = 39
       while n < 52
-        expect(presenter.display(n)).to eq 'diamonds'
+        expect(presenter.display(n)).to include 'diamonds'
         n += 1
+      end
+    end
+  end
+
+  describe 'number' do
+    it 'should know when a card is an ace' do
+      n = 0
+      while n < 40
+        expect(presenter.display(n)).to include 'ace'
+        n += 13
+      end
+    end
+
+    it 'should know when a card is a two' do
+      n = 1
+      while n < 41
+        expect(presenter.display(n)).to include 'two'
+        n += 13
+      end
+    end
+
+    it 'should know when a card is a three' do
+      n = 2
+      while n < 42
+        expect(presenter.display(n)).to include 'three'
+        n += 13
+      end
+    end
+
+    it 'should know when a card is a two' do
+      n = 3
+      while n < 43
+        expect(presenter.display(n)).to include 'four'
+        n += 13
+      end
+    end
+
+    it 'should know when a card is a two' do
+      n = 4
+      while n < 44
+        expect(presenter.display(n)).to include 'five'
+        n += 13
+      end
+    end
+
+    it 'should know when a card is a two' do
+      n = 5
+      while n < 45
+        expect(presenter.display(n)).to include 'six'
+        n += 13
+      end
+    end
+
+    it 'should know when a card is a two' do
+      n = 6
+      while n < 46
+        expect(presenter.display(n)).to include 'seven'
+        n += 13
+      end
+    end
+
+    it 'should know when a card is a two' do
+      n = 7
+      while n < 47
+        expect(presenter.display(n)).to include 'eight'
+        n += 13
+      end
+    end
+
+    it 'should know when a card is a two' do
+      n = 8
+      while n < 48
+        expect(presenter.display(n)).to include 'nine'
+        n += 13
+      end
+    end
+
+    it 'should know when a card is a two' do
+      n = 9
+      while n < 49
+        expect(presenter.display(n)).to include 'ten'
+        n += 13
+      end
+    end
+
+    it 'should know when a card is a two' do
+      n = 10
+      while n < 50
+        expect(presenter.display(n)).to include 'jack'
+        n += 13
+      end
+    end
+
+    it 'should know when a card is a two' do
+      n = 11
+      while n < 51
+        expect(presenter.display(n)).to include 'queen'
+        n += 13
+      end
+    end
+
+    it 'should know when a card is a two' do
+      n = 12
+      while n < 52
+        expect(presenter.display(n)).to include 'king'
+        n += 13
       end
     end
   end
