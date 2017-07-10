9ee432d9977aa2c52977cd87d0fee514ee339a20
Paula Muldoon <pmuldoon@umich.edu>
2017-07-10 12:52:39 +0100

    all suite tests pass

--- spec/presenter_spec.rb
+++ spec/presenter_spec.rb
@@ -5,10 +5,34 @@ describe Presenter do
   let(:presenter) { Presenter.new }
 
   describe 'suite' do
-    it 'should know what suite a card is' do
+    it 'should know when a card is a heart' do
       n = 0
       while n < 13
         expect(presenter.display(n)).to eq 'hearts'
+        n += 1
+      end
+    end
+
+    it 'should know when a card is a club' do
+      n = 13
+      while n < 26
+        expect(presenter.display(n)).to eq 'clubs'
+        n += 1
+      end
+    end
+
+    it 'should know when a card is a spade' do
+      n = 26
+      while n < 39
+        expect(presenter.display(n)).to eq 'spades'
+        n += 1
+      end
+    end
+
+    it 'should know when a card is a diamond' do
+      n = 39
+      while n < 52
+        expect(presenter.display(n)).to eq 'diamonds'
         n += 1
       end
     end
